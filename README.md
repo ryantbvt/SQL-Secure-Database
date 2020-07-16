9e9a82aa98256c83ac3d5900ff0868d00e6fe13b
ryantbvt <57875378+ryantbvt@users.noreply.github.com>
2020-07-16 14:22:35 -0400

    start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SQL-Secure-Database
+
+Goal of the project is to learn how to use sqlite and how to stop people from easily accessing information from the database by using password protection and 2-factor authentication (sending an email to the user to send a random 4-digit code that the user has to input)
--- database.py
+++ database.py
@@ -1,1 +1,23 @@
 import sqlite3
+
+# password
+PASSWORD = 'password'
+
+pass_input = input('What is your password?\n')
+
+# while the input isn't the password, ask again
+while pass_input != PASSWORD:
+    # counter variable to prevent brute force (3 tries)
+    i = 0
+    i = i + 1
+    print('Wrong password, try again')
+    pass_input = input('What is your password?\n')
+
+    if i == 3:
+        break
+
+# send email for 2-factor auth
+
+# if 2-factor auth is incorrect, deny access
+
+# if 2-factor auth is correct open database
